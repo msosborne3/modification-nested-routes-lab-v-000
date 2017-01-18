8fd5f0f827ce5587b1334a53c9e604df2e42dad1
msosborne3 <msosborne@ymail.com>
2017-01-18 14:30:29 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,14 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    # if user goes through /artists/1/songs/new for example, but the artist
+    # doesn't exist, redirect to list of artists.
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found."
+    else
+      # otherwise create a new song with the artist_id already set
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -39,7 +46,23 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    # if user goes through /artists/1/songs/3/edit for example
+    if params[:artist_id]
+      @artist_given = true # used in a helper to tell if the user went through the nested route
+
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil? # if there is no artist matching the given id, return to artists index
+        redirect_to artists_path, alert: "Artist not found."
+      else # otherwise find the given song by the given artist
+        @song = artist.songs.find_by(id: params[:id])
+        # if the song doesn't exist, return to the list of the artists songs
+        redirect_to artist_songs_path(artist), alert: "Song not found." if @song.nil?
+      end
+    else
+      # if someone just went directly to editing the song without going through artists,
+      # find the requested song.
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
@@ -64,7 +87,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
